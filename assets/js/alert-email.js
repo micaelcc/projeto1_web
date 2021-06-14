c479d2fd9ccef06ea96b00434bf71b958b89e19c
micael <micaelcomp@alu.ufc.br>
2021-06-14 20:00:08 -0300

    logo

--- assets/js/alert-email.js
+++ assets/js/alert-email.js
@@ -3,5 +3,5 @@ function exibeAlerta(){
   var assunto = document.getElementById("input_contatos_2")
   var msg = document.getElementById("input_contatos_3")
   alert("Evento Disparado! Email \"Enviado\"\n" + "Nome: " + nome.value + "\n" + "Assunto: " + assunto.value + "\n" + "Mensagem: " + msg.value)
-  document.getElementById("input_contatos").value("")
+  
 }
