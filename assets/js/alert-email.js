d19c2aa748bb2c1fddadf78c47a221d64dd32fa1
fbrunorbn <84163782+fbrunorbn@users.noreply.github.com>
2021-06-14 19:43:08 -0300

    Create alert-email.js

---
+++ assets/js/alert-email.js
@@ -0,0 +1,7 @@
+function exibeAlerta(){
+  var nome = document.getElementById("input_contatos")
+  var assunto = document.getElementById("input_contatos_2")
+  var msg = document.getElementById("input_contatos_3")
+  alert("Evento Disparado! Email \"Enviado\"\n" + "Nome: " + nome.value + "\n" + "Assunto: " + assunto.value + "\n" + "Mensagem: " + msg.value)
+  document.getElementById("input_contatos").value("")
+}
